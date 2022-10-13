bb430480d8bae1aecf0539e7153881326de6a95c
Julia <Y650480@yandex.ru>
2022-10-13 22:16:54 +0300

    Программа для решения итоговой задачи

---
+++ Task/Program.cs
@@ -0,0 +1,34 @@
+﻿// Написать программу, которая из имеющегося массива строк 
+// формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. 
+
+
+string[] str = new string[] {"Rus", "Den", "Kaz"};
+
+int ii = 0;
+
+ for(int i = 0; i<str.Length; i++)
+{
+    if (str[i].Length<=3)
+    {
+        ii++;
+    }
+}
+
+string[] strnew = new string[ii];
+
+ii = 0;
+
+ for(int i = 0; i<str.Length; i++)
+{
+    if (str[i].Length<=3)
+    {
+        strnew[ii] = str[i];
+        ii++;
+    }
+}
+
+ for(int i = 0; i<strnew.Length; i++)
+{
+    System.Console.WriteLine(strnew[i]);      
+}
